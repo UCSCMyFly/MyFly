e7befcca18bbe64e38b9f3090f24dec5f14d33a4
Rion Gronberg <rgronber@ucsc.edu>
2016-12-04 15:31:39 -0800

    add functionality for removing airports as src/dst

--- controllers/default.py
+++ controllers/default.py
@@ -82,9 +82,6 @@ def get_flights(date, flight_set):
     }
     response = requests.post(url, data=json.dumps(data), headers=headers)
     results = response.json()
-
-    #logger.info('%r', results)
-
     if 'error' in results or 'tripOption' not in results['trips']:
         logger.info('%r\n\n', results)
         logger.info('%r to %r failed', flight_set['from'], flight_set['to'])
@@ -169,6 +166,38 @@ def manage():
             response.flash = 'form two accepted'
 
     return dict(form1=form1, form2=form2)
+
+@auth.requires_login()
+def delete():
+    sources = []
+    destinations = []
+    unode = db.user_nodes(user_email = auth.user.email)
+    if unode:
+        sources = unode.sources
+        destinations = unode.destinations
+
+    form1 = FORM(INPUT(_name='name'),
+                 INPUT(_type='submit'))
+    form2 = FORM(INPUT(_name='name'),
+                 INPUT(_type='submit'))
+    if form1.process(formname='form_one').accepted:
+        sources = unode.sources
+        if request.vars.name in sources:
+            sources.remove(request.vars.name)
+            db(db.user_nodes.user_email==auth.user.email).update(sources=sources)
+            redirect(URL('default', 'delete'))
+    if form2.process(formname='form_two').accepted:
+        destinations = unode.destinations
+        if request.vars.name in destinations:
+            destinations.remove(request.vars.name)
+            db(db.user_nodes.user_email==auth.user.email).update(destinations=destinations)
+            redirect(URL('default', 'delete'))
+    return dict(srcs=sources,
+                dsts=destinations,
+                form1=form1,
+                form2=form2,
+                )
+
 
 def user():
     """
